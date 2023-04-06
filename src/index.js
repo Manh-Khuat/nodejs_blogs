57cdafda4e5dc00f0daa78db6f0ce3c3cc74f5e7
Manh-Khuat <manhkd@trithucmoi.co>
2023-04-06 14:09:14 +0700

    add bootstrap

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ const express = require('express')
 const morgan = require('morgan')
 const handlebars = require('express-handlebars')
 const app = express()
-const port = 3000
+const port = 3001
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 // HTTP logger
 app.use(morgan('combined'))
@@ -16,7 +18,7 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
 // ĐỊnh nghĩa tuyến đường
-app.get('/trang-chu', (req, res) => {
+app.get('/', (req, res) => {
   res.render('home');
 })
 
