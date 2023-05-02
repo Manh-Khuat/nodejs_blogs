1fde0a94b898954e76fdedbe8c4d8ea5a2b000f4
Manh-Khuat <manhkd@trithucmoi.co>
2023-05-02 22:06:28 +0700

    Install prettier, lint-staged, husky

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-const path = require("path");
-const express = require("express");
-const morgan = require("morgan");
-const handlebars = require("express-handlebars");
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
 const app = express();
 const port = 3002;
 
 const route = require('./routes');
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // gửi dưới dạng form
 app.use(
-  express.urlencoded({
-    extended: true,
-  })
+    express.urlencoded({
+        extended: true,
+    }),
 );
 
 // gửi từ code js
@@ -24,18 +24,18 @@ app.use(express.json());
 
 // Template engine
 app.engine(
-  "hbs",
-  handlebars.engine({
-    extname: "hbs",
-  })
+    'hbs',
+    handlebars.engine({
+        extname: 'hbs',
+    }),
 );
-app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "resources/views"));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
 
 // routes init
 route(app);
 
 // 127.0.0.1
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,10 @@ const newsRouter = require('./news');
 const siteRouter = require('./site');
 
 function route(app) {
-  // ĐỊnh nghĩa tuyến đường router
-  app.use('/news', newsRouter);
-  
-  app.use('/', siteRouter);
+    // ĐỊnh nghĩa tuyến đường router
+    app.use('/news', newsRouter);
 
+    app.use('/', siteRouter);
 }
 
 module.exports = route;
