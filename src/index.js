789014e9422e3adbef3717ccbffeeaf253b9c750
Manh-Khuat <manhkd@trithucmoi.co>
2023-05-11 16:49:14 +0700

    fix bug

--- src/index.js
+++ src/index.js
@@ -34,12 +34,12 @@ app.engine(
     }),
 );
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources/views'));
+app.set('views', path.join(__dirname, 'resources' , 'views'));
 
 // routes init
 route(app);
 
 // 127.0.0.1
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.log(`App listening on port ${port}`);
 });
